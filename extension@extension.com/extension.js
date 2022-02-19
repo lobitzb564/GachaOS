b344ec2e76aadcd08a4d3e585241455816896b68
lobitzb564 <56606893+lobitzb564@users.noreply.github.com>
2022-02-18 23:49:36 -0500

    Add files via upload

--- extension@extension.com/extension.js
+++ extension@extension.com/extension.js
@@ -1,18 +1,47 @@
 /*THIS IS THE JAVASCRIPT.COM*/
-const {St, Clutter, GLib, GObject, Shell} = imports.gi;
+const {St, Clutter, GLib, GObject, Shell, Gio} = imports.gi;
 const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 
 const HEIGHT = 50;
 let panelButton, panelButtonText, testButton;
 let lastwm;
+let lastind = 0;
 let appSystem, appStateChangedid;
 let container, box, layout, cntnr;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+async function pinthis() {
+    let fileexists = Me.dir.get_child("pins.txt").query_exists(null);
+    if (!fileexists) {
+
+    }
+
+    let file = Gio.File.new_for_path(Me.path + "/pins.txt");
+    
+    let [, contents, fetag] = await new Promise((resolve, reject) => {
+        file.load_contents_async(
+            null,
+            (file_, result) => {
+                try {
+                    resolve(file.load_contents_finish(result));
+                } catch(e) {
+                    reject(e);
+                }
+            }
+        );
+    });
+    let tabs = contents.toString().split('\n');
+    for (let i = 0; i < tabs.length; i++) {
+        Main.panel._leftBox.insert_child_at_index(new St.Button({label: tabs[i]}), i);
+    }
+    return contents;
+}
 
 var Tab = GObject.registerClass({
     GtypeName: "Tab",
 }, class Tab extends St.Button {
-    _init(app) {
+    _init(app, index) {
         this.app = app;
         super._init({
             style_class: 'examplePanelText',
@@ -22,11 +51,11 @@ var Tab = GObject.registerClass({
         });
         this.icon = new St.Icon({gicon: this.app.get_icon()});
         this.set_child(this.icon);
-        this.connect("clicked", this.onclick.bind(this));
+        this.connect("clicked", this.clicked.bind(this));
         this.connect('destroy', this.onDestroy.bind(this));
         this.connect('style-changed', this.mouseov.bind(this));
     }
-    onclick(clicked_button) {
+    clicked(clicked_button) {
         let btnlist = this.app.get_windows();
         if (btnlist.length <= 1){btnlist[0].make_above();
         } else {
@@ -35,12 +64,21 @@ var Tab = GObject.registerClass({
         }
     }
 
+    pintab() {
+        let contents = pinthis();
+        let newfstr = contents.toString();
+        if (!contents.toString().includes(obj.app.get_id())) {
+            newfstr += this.app.get_id()+"\n";
+        }
+        let [, etag] = file.replace_contents(newfstr, null, false, Gio.FileCreateFlags.REPLACE_DESTINATION, null);
+    }
+    
+
     mouseov() {
     if (this.hover) {
-        this.remove_all_children();
         this.set_label(this.app.get_name());
     } else {
-    this.set_child(this.icon);
+        this.set_child(this.icon);
     }
     }
 
@@ -53,6 +91,25 @@ var Tab = GObject.registerClass({
     }
 }
 );
+
+var Pinned = GObject.registerClass({
+    GTypeName: "Pinned"
+}, class Pinned extends Tab {
+    _init(appID, index) {
+        this.app = Shell.AppSystem.lookup_app(appID);
+        this.index = index;
+        if (this.app) {
+            super._init(this.app, this.index);
+            this.connect("clicked", this.clicked.bind(this));
+        }
+    }
+
+    clicked(clicked_button) {
+        this.app.launch();
+        this.destroy;
+        cntnr.insert_child_at_index(new Tab(this.app, this.index));
+    }
+});
 
 var Popup = GObject.registerClass({
     GtypeName: "Popup",
@@ -68,11 +125,24 @@ var Popup = GObject.registerClass({
             height: ((btnlist.length*30)),
             width: 190,
         });
+        this.big = new St.Button({
+            style_class: 'bigblk',
+            reactive: true,
+            height: Main.layoutManager.primaryMonitor.height,
+            width: Main.layoutManager.primaryMonitor.width 
+        });
+        Main.layoutManager.addChrome(this.big, {
+            affectsInputRegion: true,
+            affectsStruts: false,
+            trackFullscreen: true,
+        });
         Main.layoutManager.addChrome(this, {
             affectsInputRegion: true,
             affectsStruts: false,
             trackFullscreen: true,
         });
+        this.big.connect('clicked', this.outsideclk.bind(this));
+        this.big.set_position(0, 0);
         let bx =new St.BoxLayout({vertical: true});
         this.add_actor(bx);
         this.set_position(xpos, Main.layoutManager.primaryMonitor.height-HEIGHT-this.height);
@@ -82,6 +152,12 @@ var Popup = GObject.registerClass({
             bx.insert_child_at_index(p, 0);
         }
         //this.connect("clicked", this.onclick.bind(this));
+    }
+
+
+    outsideclk(clicked_button) {
+        Main.layoutManager.removeChrome(this);
+        Main.layoutManager.removeChrome(this.big);
     }
   }
 );
@@ -103,12 +179,12 @@ var Popupwin = GObject.registerClass({
     onclick(clicked_button) {
         this.win.make_above();
         Main.layoutManager.removeChrome(this.parent);
+        Main.layoutManager.removeChrome(this.parent.big);
     }
 }
 );
 
 function init() {
-    
     let monitor = Main.layoutManager.primaryMonitor;
     layout = new Clutter.BoxLayout({homogeneous: true});
     container = new St.Widget({
@@ -158,6 +234,8 @@ function enable() {
         affectsStruts: true,
         trackFullscreen: true,
     });
+
+    pinthis();
 }
 
 function disable() {
@@ -167,9 +245,10 @@ function disable() {
 
 function onChange(appSys, app) {
     if (app.state === Shell.AppState.RUNNING) {
-        let btn = new Tab(app);
+        let btn = new Tab(app, lastind);
         btn.set_track_hover(true);
-        cntnr.insert_child_at_index(btn, 0);
+        cntnr.insert_child_at_index(btn, lastind);
+        lastind++;
     } else if (app.state === Shell.AppState.STOPPED) {
         let children = cntnr.get_children();
         let child = children.find(c => c.getapp() === app);
