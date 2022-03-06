ed3e3d8775762269370bb579d81ff9b3882b9be6
Dev Amlani <amlanid35@gmail.com>
2022-03-06 15:05:54 -0500

    ...

---
+++ spinning_wheel/extension.js
@@ -0,0 +1,14 @@
+const ExtensionUtils = imports.misc.ExtensionUtils;
+class Extension{
+    constructor(){
+    
+    }
+    enable(){
+
+    }
+    disable(){}
+}
+function init(meta){
+    return new Extension();
+
+}
